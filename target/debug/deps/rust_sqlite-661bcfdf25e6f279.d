f6249a89a901f09119f06a85ac4ccf1baf6b8f32
Laura Pilkington <4733367+Laurelin@users.noreply.github.com>
2023-03-13 11:05:19 -0400

    init commit

---
+++ target/debug/deps/rust_sqlite-661bcfdf25e6f279.d
@@ -0,0 +1,5 @@
+/Users/lpilkington/GitHub/rust-sqlite/target/debug/deps/rust_sqlite-661bcfdf25e6f279: src/main.rs
+
+/Users/lpilkington/GitHub/rust-sqlite/target/debug/deps/rust_sqlite-661bcfdf25e6f279.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-sqlite.d
@@ -0,0 +1,1 @@
+/Users/lpilkington/GitHub/rust-sqlite/target/debug/rust-sqlite: /Users/lpilkington/GitHub/rust-sqlite/src/main.rs
